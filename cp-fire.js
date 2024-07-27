f9ebfe9ee05cd6832b8144ad45334a3d73d26c26
businesssuccess <159882838+businesssuccess@users.noreply.github.com>
2024-07-27 14:39:40 +1000

    Update cp-fire.js

--- cp-fire.js
+++ cp-fire.js
@@ -1,18 +1,20 @@
-// Minimal Firebase Initialization
-const firebaseConfig = {
-  apiKey: "AIzaSyA4h7CLTXk4nZ_4-oB7Jva_tpLGT9OtfrU",
-  authDomain: "cf-obvio-link.firebaseapp.com",
-  databaseURL: "https://cf-obvio-link-default-rtdb.firebaseio.com",
-  projectId: "cf-obvio-link",
-  storageBucket: "cf-obvio-link.appspot.com",
-  messagingSenderId: "795544771056",
-  appId: "1:795544771056:web:29fd38b91b33eb5f78e535",
-  measurementId: "G-98YVR7FK88"
-};
+(function() {
+    // Minimal Firebase Initialization
+    const firebaseConfig = {
+        apiKey: "YOUR_API_KEY",
+        authDomain: "YOUR_AUTH_DOMAIN",
+        databaseURL: "YOUR_DATABASE_URL",
+        projectId: "YOUR_PROJECT_ID",
+        storageBucket: "YOUR_STORAGE_BUCKET",
+        messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+        appId: "YOUR_APP_ID",
+        measurementId: "YOUR_MEASUREMENT_ID"
+    };
 
-firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
 
-// Minimal function
-window.processEvent = function() {
-  console.log("processEvent function is available.");
-};
+    // Minimal function
+    window.processEvent = function() {
+        console.log("processEvent function is available.");
+    };
+})();
